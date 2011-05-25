2871b926319a7e6169f0aa034dc6ea2afa966697
nes.manrique@gmail.com <nes.manrique@gmail.com@bd4f8b31-ceae-e714-de68-d3ef3dd9363f>
2011-05-25 23:03:08 +0000

    manhattan y estructura del nodo from hell

--- aestrella.cpp
+++ aestrella.cpp
@@ -1,16 +1,19 @@
 #include "aestrella.h"
 using namespace std;
 
-short * lookup[2];
+short lookup[16][2] = {{0,0},{0,1},{0,2},{0,3},{1,0},{1,1},{1,2},{1,3},{2,0},{2,1},{2,2},{2,3},{3,0},{3,1},{3,2},{3,3}};
 
-short puzzle::manhattan(State s){
+short manhattan(State s){
 
-    short i,j,n,r;
+    unsigned short i,j,r;
+    unsigned int n;
     r = 0;
-    for(i=0; i<N && i!=s.whitex; i++){
-        for(j=0; j<N && j!=s.whitey; j++){
-            n = s.tablero[i][j];
-            r = r + abs(lookup[n][0]-i + lookup[n][1]-j);
+    for(i=0; i<4; i++){
+        for(j=0; j<4; j++){
+            n = s.tablero[i] & masks[j];
+            n >>= ((3-j)<<2);
+            r = r + abs(lookup[n][0]-i) + abs(lookup[n][1]-j);
+            printf("r %d\n",r);
         }
     }
 
@@ -21,6 +24,10 @@ short puzzle::heuristica(State s){
     return manhattan(s);  
 }
 
-char * puzzle::solve(State s){
-
+int main(){
+    
+    int arr[16] = {0,1,3,6,2,5,7,8,9,10,11,13,12,14,15,4};
+    puzzle p(arr, 0,0);
+    
+    return 0;
 }
