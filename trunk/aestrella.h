51ad8f63f6f0e2cb89497c0d1f75eb4b3a86b79e
nes.manrique@gmail.com <nes.manrique@gmail.com@bd4f8b31-ceae-e714-de68-d3ef3dd9363f>
2011-05-25 23:03:08 +0000

    manhattan y estructura del nodo from hell

--- trunk/aestrella.h
+++ trunk/aestrella.h
@@ -3,33 +3,50 @@
 #include <queue>
 using namespace std;
 
-template <short N> class puzzle{    
+const short masks[4] = {0xf000, 0x0f00, 0x00f0, 0x000f};
 
-    struct State{
-        short tablero[N][N];
-        short whitex;
-        short whitey;
-        short cost;
-        short steps;
-        char * sol;
-    };
+struct State{
+    unsigned short tablero [4];
+    char white;
+    short cost;
+};
+
+class puzzle{    
 
     State ini;
 
-    puzzle puzzle(short [][] t){
-        
-        short i,j;
-        for(i=0; i<N; i++){
-            for(j=0; i<N; i++){
-                
+    public:
+
+    puzzle(int t[], int whitex, int whitey){
+       
+        unsigned short i,j,k,l;
+        for(i=0, l=0; i<4; i++){
+            k =0;
+            for(j=0; j<4; j++){
+                k <<= 4;
+                k |= t[l++];
             }
+            ini.tablero[i] = k;
         }
+        
+        k =0;
+        k |= whitex;
+        k <<= 4;
+        k |= whitey;
+        ini.white = k;
+        ini.cost = heuristica(ini);
+        printf("tablero %x\n",ini.tablero[0]);
+        printf("tablero %x\n",ini.tablero[1]);
+        printf("tablero %x\n",ini.tablero[2]);
+        printf("tablero %x\n",ini.tablero[3]);
+        printf("costo %d\n",ini.cost);
     }
 
     /* Funcion que calcula la heuristica */
     short heuristica(State s);
+};
 
-    /* Funcion que expande un estado */
+    /* Funcion que expande un estado 
     void expandir(State s, State * neighbors){
 
         short i, j,n, c = 0;
@@ -84,11 +101,11 @@ template <short N> class puzzle{
     }
 
 
-    /* Chequea si se llego al goal*/
+    * Chequea si se llego al goal*
     bool isGoal(State s);
 
-    /* Algoritmo a estrella =) */
+    * Algoritmo a estrella =) *
     char * solve(State s);
 
-    /* mas nada.. (por ahora) */
-}
+    * mas nada.. (por ahora) */
+
